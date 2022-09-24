f45a711213ed55ccc61706252696a911d4966ab6
Justin Garner <j.garner828@gmail.com>
2022-09-24 18:40:03 -0400

    having websocket issues with followup messages

--- auction-client/src/components/AuctionList.js
+++ auction-client/src/components/AuctionList.js
@@ -9,115 +9,81 @@ function AuctionList({user, authCredentials, token}) {
  const [auctionItems, setAuctionItems] = useState([]);
 
  const [userData, setUserData] = useState({
-     email: user.email,
-     name: user.name,
-     message: ''
-   });
+                                            email: user.email,
+                                            name: user.name,
+                                            message: ''
+                                          });
 
+  const [bid, setBid] = useState(0.00);
 
+  let stompClient;
+  let socket;
 
-
-var stompClient = null;
-
-
-const connect = () => {
-      // eslint-disable-next-line no-undef
-      const socket = new SockJS('http://localhost:8080/ws')
-      // eslint-disable-next-line no-undef
+const connect = async () => {
+      socket = new SockJS('http://localhost:8080/ws')
       stompClient = Stomp.over(socket)
       stompClient.connect({}, onConnected, onError)
 }
 
 
-const onConnected = () => {
-  console.log("onConnected function")
+const onConnected = async() => {
 
   stompClient.subscribe('/topic/bids', onMessageReceived)
-  
 
   stompClient.send("/app/socket.newUser",
       {},
       JSON.stringify({
           sender: user.name,
-          type: 'NORMAL',
+          type: 'NEW_USER',
           time: Date.now()
       })
   )
-
 }
 
+
+
  
-const onError = (err) => {
+const onError = async (err) => {
   console.log(err);
  
 }
 
-const [bid, setBid] = useState(0.00);
 
-
-function handleChange (e) { 
+const handleChange = async (e) =>{ 
   setBid(e.target.value);
 }
 
-function submitBid (item) { 
 
+const submitBid =  async (item) => { 
+  
 
-  let newBid = {
-                item_id: item.id,
-                user_email: user.email,
-                bid_price: bid,
-                bid_time: new Date().now()
-        }
+  let newMessage = {
+                type: "BID",
+                newBid: {
+                        itemId: item.id,
+                        email: user.email,
+                        bidPrice: bid,
+                        bidTime: new Date().getTime()
+                       },
+                sender: userData.email,
+                time: new Date().getTime()
+      };
 
-
-        var stompClient = null;
-            const onError = (err) => {console.log(err);}
-            const onMessageReceived = (payload)=>{console.log(payload)}
-            const onConnected = () => {
-                                stompClient.subscribe('/topic/bids', onMessageReceived)
-                          }        
-                          // eslint-disable-next-line no-undef
-            const socket = new SockJS('http://localhost:8080/ws')
-                          // eslint-disable-next-line no-undef
-            stompClient = Stomp.over(socket)
-            stompClient.connect({}, onConnected, onError)
-
-
-            stompClient.send("/socket.send", {}, JSON.stringify(JSON.stringify(newBid)));
-
-
-
-
+            try { 
+              stompClient.send("/socket.send", {}, JSON.stringify(newMessage));
+            } catch(err){ 
+              console.log(err); }
 }
 
 
-
-const onMessageReceived = (payload)=>{
+const onMessageReceived = async (payload)=>{
+  console.log("onMessageReceived")
   console.log(payload)
 }
 
 
 
-const handleMessage =(event)=>{
-  const {value}=event.target;
-  setUserData({...userData,"message": value});
-}
-
-
-const sendValue=()=>{
-  if (stompClient) {
-    var chatMessage = {
-      senderName: userData.username,
-      message: userData.message,
-      status:"MESSAGE"
-    };
-    console.log(chatMessage);
-    stompClient.send("/app/message", {}, JSON.stringify(chatMessage));
-    setUserData({...userData,"message": ""});
-  }
-}
-
-const getAuctionList=()=>{
+const getAuctionList = async () => {
 
     const url = "http://localhost:8080/auctionlist";
   
@@ -135,23 +101,16 @@ const getAuctionList=()=>{
 };
 
 
- useEffect(() => {
+useEffect( () => {
   getAuctionList();
-   connect();
- }, []);
+  connect();
+}, []);
 
 
- useEffect(() => {
-
- }, [auctionItems]);
-
-
-  if (!auctionItems.length === 0 || userData.email == null) return null;
-
-  return (
+return (
     <ul>  
         {auctionItems.map( item =>  {
-          return(                <div className = "auctionItemComponent">
+          return(                <div key={item.id} className = "auctionItemComponent">
                 <h3>{item.name}</h3>
                 <span>{item.desc}</span>
                 <span>Current Bid: ${item.itemStartingPrice}</span>
